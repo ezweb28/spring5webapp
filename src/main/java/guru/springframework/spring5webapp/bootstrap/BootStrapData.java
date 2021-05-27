230bd056a449c098ee892a199865efee5258dfa3
Sun <ezweb28@gmail.com>
2021-05-26 23:42:23 -0400

    CH.19

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -25,30 +25,39 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Publisher sun = new Publisher("sun", "2951 satellite blvd", "Duluth", "GA", "30096");
+        publisherRepository.save(sun);
+        System.out.println("Number of publishers: " + publisherRepository.count());
+
         Author eric = new Author("Eric", "Evan");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(sun);
+        sun.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(sun);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        sun.getBooks().add(noEJB);
+        noEJB.setPublisher(sun);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+
+        publisherRepository.save(sun);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
 
-        Publisher sun = new Publisher("sun", "2951 satellite blvd", "Duluth", "GA", "30096");
-
-        publisherRepository.save(sun);
-
-        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Number of Publisher Books: " + sun.getBooks().size());
 
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -13,6 +12,10 @@ public class Publisher {
     private Long id;
 
     private String name, addressLine1, city, state, zip;
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
 
     public Publisher() {
 
@@ -24,6 +27,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public String getName() {
